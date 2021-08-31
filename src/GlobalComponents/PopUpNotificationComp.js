fba72d35c1b54c210e3410e728101cd6d8cee1fc
brajesh pal <brajeshpal466@gmail.com>
2021-08-31 18:01:26 +0530

    notification removed

--- src/GlobalComponents/PopUpNotificationComp.js
+++ src/GlobalComponents/PopUpNotificationComp.js
@@ -4,9 +4,7 @@ import React from 'react'
 function PopUpNotificationComp({width,height}) {
     return (
         <>
-         <Flex zIndex="10" width={width} backgroundColor="wheat"  height={height} position="fixed" right="0px" top="0px">
-             
-             </Flex>   
+          
         </>
     )
 }
