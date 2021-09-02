d75dc59d7ab91a5ab751b719a13742a26b68b626
brajesh pal <brajeshpal466@gmail.com>
2021-09-02 07:52:29 +0530

    select updated

--- src/GlobalComponents/SelectComp.js
+++ src/GlobalComponents/SelectComp.js
@@ -5,8 +5,8 @@ import { Select } from "@chakra-ui/react"
 function SelectComp({placeHolder,optionList,padding}) {
     return (
         <>
-       <Select placeholder={placeHolder} padding={padding} type="date" >
-     {  optionList.map((option,index)=> {
+       <Select placeholder={placeHolder} variant="filled" size="md"   border="1px solid grey" outline="none" height="26px">
+     { optionList && optionList.map((option,index)=> {
          return(<option id={index} >{option}</option>);
      }
      
