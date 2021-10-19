f54642d503faafb11449dbdd3b41b20075782d4b
brajesh pal <brajeshpal466@gmail.com>
2021-10-19 12:03:41 +0530

    experince kpi resposive and with tooltip prepare

--- src/app/pages/ExperienceKpi/Features/index.tsx
+++ src/app/pages/ExperienceKpi/Features/index.tsx
@@ -13,49 +13,117 @@ const data = [
   [22, 24],
 ];
 
-function draw(exp) {
+let arr = [];
+let columnPos = [];
+
+let relativepos;
+
+// draw for particular ploting area
+function drawArea(exp, width, height, item) {
+  let slide = width / 26;
+
+  exp.beginPath();
+  exp.moveTo(slide * item[0], 0); // starting point (left ,top)
+  exp.lineTo(slide * item[0], height); // bottom point (left ,top)
+  exp.lineTo(slide * item[1], height); // bottom starting point  (left,top)
+  exp.lineTo(slide * item[1], 0); //    last point (left , top)
+  exp.strokeStyle = '#CEF2FE';
+  exp.fill();
+  exp.stroke();
+  let left = relativepos.left;
+  //console.log( relativepos.width/26);
+  //console.log(width/26);
+
+  arr.push({
+    left: left,
+    start: (relativepos.width / 26) * item[0] + left,
+    end: (relativepos.width / 26) * item[1] + left,
+    val: item,
+    tippos: (relativepos.width / 26) * item[0],
+  });
+}
+
+// draw method of canvas
+
+function draw(exp, tipRef, w, h) {
   const canvas = exp.canvas;
   exp.fillStyle = 'transparent';
+  let tipElement = tipRef.current;
 
-  let slide = canvas.width / 26;
-  let height = canvas.height;
-  exp.fillRect(0, 0, canvas.width, canvas.height);
-  //console.log(slide);
-  //console.log(canvas.height);
+  ////console.log(tipElement);
 
+  exp.fillRect(0, 0, w, h);
+
+  arr = [];
+  columnPos = [];
   exp.fillStyle = '#CEF2FE';
 
   data.map(item => {
-    exp.beginPath();
-    exp.moveTo(slide * item[0], 0); // starting point (left ,top)
-    exp.lineTo(slide * item[0], height); // bottom point (left ,top)
-    exp.lineTo(slide * item[1], height); // bottom starting point  (left,top)
-    exp.lineTo(slide * item[1], 0); //    last point (left , top)
-    exp.strokeStyle = '#CEF2FE';
-    exp.fill();
-    exp.stroke();
+    drawArea(exp, w, h, item);
   });
+}
+function hoverColumn(x) {
+  let hoveredCol = arr.filter(item => item.start < x && x < item.end)[0];
 
-  requestAnimationFrame(() => draw(exp));
+  return hoveredCol;
 }
 
 function ExpChart() {
-  const expC = useRef();
+  const expRef = useRef();
+  const tipRef = useRef();
 
   useEffect(() => {
-    if (expC.current) {
-      let exp = expC.current.getContext('2d');
-
-      requestAnimationFrame(() => draw(exp));
+    let exp = expRef.current.getContext('2d');
+    if (expRef.current) {
+      relativepos = expRef.current.getBoundingClientRect();
+      draw(exp, tipRef, window.innerWidth, window.innerHeight);
       const handleResize = () => {
+        relativepos = expRef.current.getBoundingClientRect();
+        //  //console.log(exp);
         exp.canvas.height = window.innerHeight;
         exp.canvas.width = window.innerWidth;
+        draw(exp, tipRef, window.innerWidth, window.innerHeight);
       };
+
       handleResize();
+
+      const mouseHandler = e => {
+        if (expRef.current) {
+          const tooltip = tipRef.current.getContext('2d');
+          tooltip.fillStyle = '#ffffff';
+          tooltip.font = '12px Verdana';
+          tipRef.current.position = 'absolute';
+
+          const val = hoverColumn(e.clientX);
+
+          if (val && e.clientX < 1300) {
+            //console.log(val.val);
+
+            tipRef.current.style.display = 'block';
+            tipRef.current.style.left = val.tippos + 'px';
+            tooltip.save();
+            ////console.log(tooltip);
+            tooltip.clearRect(0, 0, 100, 50);
+            tooltip.fillRect(0, 0, 100, 50);
+            tooltip.fillStyle = '#000000';
+            tooltip.fillText(val.val, 10, 15);
+            tooltip.restore();
+          } else {
+            tipRef.current.style.display = 'none';
+          }
+        }
+      };
+      expRef.current.addEventListener('mousemove', mouseHandler);
+
       window.addEventListener('resize', handleResize);
 
-      return () => window.removeEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('mousemove', mouseHandler);
+        window.removeEventListener('resize', handleResize);
+      };
     }
+
+    //
   }, []);
 
   return (
@@ -63,7 +131,18 @@ function ExpChart() {
       <canvas
         id="exp"
         style={{ width: '100%', height: '100%' }}
-        ref={expC}
+        ref={expRef}
+      ></canvas>
+      <canvas
+        ref={tipRef}
+        height={50}
+        width={100}
+        style={{
+          position: 'absolute',
+          background: 'transparent',
+          display: 'none',
+          zIndex: '2',
+        }}
       ></canvas>
     </>
   );
